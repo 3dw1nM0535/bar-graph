57c1228e768a63e1b83fc65ca5da7f0499f43efc
Edwin Moses <workockmoses@gmail.com>
2017-06-28 14:37:23 +0300

    d3 phase implementation

--- src/data-vis.js
+++ src/data-vis.js
@@ -11,13 +11,21 @@ var canvas = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
 
-canvas.selectAll("rect")
-  .data(data)
-  .enter()
-    .append("rect")
-    .attr("x", scale)
+function render(data, color) {
+  //Bind data
+  var rect = canvas.selectAll("rect").data(data);
+
+  //Enter Phase
+  rect.enter().append("rect")
     .attr("y", 50)
     .attr("width", 50)
-    .attr("height", 50)
-    .attr("fill", "ForestGreen");
+    .attr("height", 50);
+
+  //Updata Phase
+  rect.attr("x", scale).attr("fill", color);
+
+  //Exit Phase
+  rect.exit().remove();
+  
+}
 
