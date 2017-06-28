5f1244c59dcd8ac4bc0c19b6a03c95038efdb33e
Edwin Moses <workockmoses@gmail.com>
2017-06-28 15:31:19 +0300

    scale variables

--- src/data-vis.js
+++ src/data-vis.js
@@ -5,8 +5,8 @@ var canvas = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
 
-var xScale = d3.scale.linear().range([0, 250]);
-var yScale = d3.scale.linear().range([0, 250]);
+var xScale = d3.scale.linear().range([0, 500]);
+var yScale = d3.scale.linear().range([0, 500]);
 
 function render(data) {
   xScale.domain();
