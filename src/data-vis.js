d946a1f4f8603386c7a727f6597f8b4da7e87dbb
Edwin Moses <workockmoses@gmail.com>
2017-06-28 13:38:11 +0300

    adding bar graph

--- src/data-vis.js
+++ src/data-vis.js
@@ -5,8 +5,3 @@ var canvas = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
 
-var circle = canvas.append("circle")
-  .attr("cx", 150)
-  .attr("cy", 150)
-  .attr("r", 100)
-  .attr("fill", "DeepPink");
