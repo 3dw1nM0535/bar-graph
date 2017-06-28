0c3a5ecb50b18ecf6d418538ce41fe7281b5fce6
Edwin Moses <workockmoses@gmail.com>
2017-06-28 15:45:34 +0300

    domain scaling

--- src/data-vis.js
+++ src/data-vis.js
@@ -9,7 +9,23 @@ var xScale = d3.scale.linear().range([0, 500]);
 var yScale = d3.scale.linear().range([0, 500]);
 
 function render(data) {
-  xScale.domain();
-  yScale.domain();
+  xScale.domain(d3.extent(data, function(d) { return d.sepal_length; }));
+  yScale.domain(d3.extent(data, function(d) { return d.petal_length; }));
+
+  var circles = canvas.selectAll("circle").data(data);
+  circles.enter().append("circle").attr("r", 10);
+  circles.attr("cx", function(d) { return xScale(d.sepal_length); }).attr("cy", function(d) { return yScale(d.petal_length); });
+
+  circles.exit().remove();
 
 }
+
+function type(d) {
+  d.sepal_length = +d.sepal_length;
+  d.petal_length = +d.petal_length;
+  d.sepal_width = +d.sepal_width;
+  d.petal_width = +d.petal_width;
+  return d;
+}
+
+d3.csv("/data.csv", type, render);
