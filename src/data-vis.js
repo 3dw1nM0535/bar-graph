ba468fde80df8824c0ef6fc1ab7c8da3bc939c72
Edwin Moses <workockmoses@gmail.com>
2017-06-28 15:29:46 +0300

    scatterplot graph

--- src/data-vis.js
+++ src/data-vis.js
@@ -5,26 +5,11 @@ var canvas = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
 
+var xScale = d3.scale.linear().range([0, 250]);
+var yScale = d3.scale.linear().range([0, 250]);
+
 function render(data) {
-  //Binding data
-  var circles = canvas.selectAll("circle").data(data);
+  xScale.domain();
+  yScale.domain();
 
-  //Enter Phase
-  circles.enter().append("circle").attr("r", 15);
-
-  //Updata Phase
-  circles.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-
-  circles.exit().remove();
-};
-
-var objectArray = [
-  { x: 100, y: 100 },
-  { x: 130, y: 120 },
-  { x: 80, y: 180  },
-  { x: 180, y: 80  },
-  { x: 180, y: 40  }
-];
-
-render(objectArray);
+}
