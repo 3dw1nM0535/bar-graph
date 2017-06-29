04700fe9c4d5e6673618837c541521d60720513f
Edwin Moses <workockmoses@gmail.com>
2017-06-29 13:41:04 +0300

    bar scaling

--- src/data-vis.js
+++ src/data-vis.js
@@ -1,19 +1,17 @@
 var width = 500;
 var height = 500;
-var data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+var data = [1, 2, 3, 4, 5];
 
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
 
-var widthScale = d3.scale.linear().domain([0, 100]).range([0, width]);
+var scale = d3.scale.linear().domain([1, 5]).range([0, 500]);
 
-var bars = svg.selectAll("rect").data(data);
-
-bars.enter().append("rect").attr("height", 50);
-
-bars
-  .attr("width", function(d) { return widthScale(d); })
-  .attr("y", function(d, i) { return i * 100; });
-
-bars.exit().remove();
+svg.selectAll("rect")
+  .data(data)
+  .enter().append("rect")
+    .attr("x", function(d) { return scale(d); })
+    .attr("y", 50)
+    .attr("width", 50)
+    .attr("height", 50);
