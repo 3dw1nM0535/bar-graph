fa3bbc9e1c7a5080c2766525b3a3d1d09c90d3a8
Edwin Moses <workockmoses@gmail.com>
2017-06-29 21:09:40 +0300

    data visualization

--- src/data-vis.js
+++ src/data-vis.js
@@ -6,4 +6,20 @@ var svg = d3.select("body").append("svg")
   .attr("height", height)
 
 d3.json("/myData.json", function(data) {
+
+  const data_Items = [12, 31, 22, 17, 25, 18, 29, 14, 9, 23, 43, 5, 1, 21];
+  //Binding data
+  const rects = svg.selectAll("rect").data(data_Items);
+  //Enter
+  rects.enter().append("rect")
+    .attr("y", 0)
+    .attr("width", 25)
+    .attr("fill", "crimson");
+
+  rects
+    .attr("height", function(d) { return d * 3; })
+    .attr("x", function(d, i) { return i * 50; });
+    
+
+
 });
