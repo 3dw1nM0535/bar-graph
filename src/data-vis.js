fdabe21fea7767160fb3d12a390ee669d8b4b092
Edwin Moses <workockmoses@gmail.com>
2017-06-29 18:16:26 +0300

    data grep from array of arrays

--- src/data-vis.js
+++ src/data-vis.js
@@ -5,6 +5,7 @@ var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
 
-d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function(data) {
-  console.log(data.data);
+d3.json("/myData.json", function(data) {
+  var data_item = data.data.map(function(d) { return d[1]; });
+  var data_Dates = data.data.map(function(d) { return d[0]; });
 });
