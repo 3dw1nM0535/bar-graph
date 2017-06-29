7f13d098dc0e8b4231874c4372a54b49e9232bf2
Edwin Moses <workockmoses@gmail.com>
2017-06-29 13:15:53 +0300

    bar graphs using rects

--- src/data-vis.js
+++ src/data-vis.js
@@ -1,7 +1,17 @@
 var width = 500;
 var height = 500;
+var data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height)
 
+var bars = svg.selectAll("rect").data(data);
+
+bars.enter().append("rect").attr("height", 50);
+
+bars
+  .attr("width", function(d) { return d * 10; })
+  .attr("y", function(d, i) { return i * 100; });
+
+bars.exit().remove();
