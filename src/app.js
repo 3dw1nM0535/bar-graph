4be242560eab577071e621fd6f636a6852d4bc3f
Edwin Moses <workockmoses@gmail.com>
2017-06-30 01:21:13 +0300

    scale cut

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ const canvas = d3.select(".container").append("svg")
   .attr("width", w)
   .attr("height", h);
 
-const xScale = d3.scaleLinear().range([0, 700]);
-const yScale = d3.scaleLinear().range([0, 700]);
+
 
 d3.json("/myData.json", function(data) {
-  var dataset = data.data.map(function(d) { return d; });
-  xScale.domain(d3.extent(dataset, (d) => new Date(d[0]).getFullYear()));
-  yScale.domain(d3.extent(dataset, (d) => parseFloat(d[1])));
+  const dataset = data.data.map(function(d) { return d; });
+
+  const xScale = d3.scaleLinear().range([0, 700]).domain(d3.extent(dataset, (d) => new Date(d[0]).getFullYear()));
+  const yScale = d3.scaleLinear().range([0, 700]).domain(d3.extent(dataset, (d) => parseFloat(d[1])));
 });
