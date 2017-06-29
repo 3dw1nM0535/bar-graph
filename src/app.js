db69d09a1e669fd125bcbeb5280ef46a3aea5db2
Edwin Moses <workockmoses@gmail.com>
2017-06-30 01:18:12 +0300

    scale conversion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const w = 700;
 const h = 500;
 
-var canvas = d3.select(".container").append("svg")
+const canvas = d3.select(".container").append("svg")
   .attr("width", w)
   .attr("height", h);
+
+const xScale = d3.scaleLinear().range([0, 700]);
+const yScale = d3.scaleLinear().range([0, 700]);
+
+d3.json("/myData.json", function(data) {
+  var dataset = data.data.map(function(d) { return d; });
+  xScale.domain(d3.extent(dataset, (d) => new Date(d[0]).getFullYear()));
+  yScale.domain(d3.extent(dataset, (d) => parseFloat(d[1])));
+});
