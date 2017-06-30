7daec7ced0dd61c03c1466e9e01b0ebbcfbfac48
Edwin Moses <workockmoses@gmail.com>
2017-06-30 17:58:31 +0300

    bar graph

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 var w = 1000;
-var h = 700;
+var h = 500;
+
+var canvas = d3.select(".container").append("svg")
+  .attr("width", w)
+  .attr("height", h)
+
+var rect = canvas.append("rect")
+  .attr("width", w)
+  .attr("height", h)
