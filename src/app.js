919d47884a7a60dabd92681cf2ff19569e3e1827
Edwin Moses <workockmoses@gmail.com>
2017-07-01 11:23:13 +0300

    added data scale

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ var canvas = d3.select(".container").append("svg")
 d3.json("/myData.json", function (data) {
   console.log(data)
 
-
   d3.select("h4").text(data.name);
+  d3.select(".container").append("p").text(data.description);
+
+  var toolTip = d3.select(".container").append("div").attr("class", "toolTip");
+  var yScale = d3.scaleLinear().domain([0, d3.max(data.data, (d) => d[1])]).range([0, h]);
+  var xAxisScale = d3.scaleLinear().domain([d3.extent(data.data, (d) => new Date(d[0]).getFullYear())]).range([0, w - 100]);
+  var yAxisScale = d3.scaleLinear().domain([0, d3.max(data.data, (d) => d[1])]).range([h - 50, 0]);
 })
