ed76b1bb5474e0d33993b770cc1ee5a8a1808090
Edwin Moses <workockmoses@gmail.com>
2017-07-01 11:37:21 +0300

    added colorScale()

--- src/app.js
+++ src/app.js
@@ -15,4 +15,16 @@ d3.json("/myData.json", function (data) {
   var yScale = d3.scaleLinear().domain([0, d3.max(data.data, (d) => d[1])]).range([0, h]);
   var xAxisScale = d3.scaleLinear().domain([d3.extent(data.data, (d) => new Date(d[0]).getFullYear())]).range([0, w - 100]);
   var yAxisScale = d3.scaleLinear().domain([0, d3.max(data.data, (d) => d[1])]).range([h - 50, 0]);
+  var colorScale = d3.scaleLinear().domain([0, d3.max(data.data, (d) => d[1])]).range(["#ffe6e6", "#1a0000"]);
+
+  //Binding data
+  var rects = canvas.append("g").attr("transform", "translate(50, 50)").selectAll("rect").data(data.data);
+
+  //Enter
+  rects.enter().append("rect")
+    .attr("width", (w - 100)/data.data.length)
+    .attr("height", (d) => yScale(d[1]))
+    .attr("y", (d) => h - 50 - yScale(d[1]))
+    .attr("fill", (d) => colorScale(d[1]))
+    .attr("x", (d, i) => i * (w - 100)/data.data.length);
 })
