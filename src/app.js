16371a6793775ea5913a0e1cc8a05e19e0608349
Edwin Moses <workockmoses@gmail.com>
2017-07-01 11:01:07 +0300

    added data title label

--- src/app.js
+++ src/app.js
@@ -3,4 +3,11 @@ var h = 500;
 
 var canvas = d3.select(".container").append("svg")
   .attr("width", w)
-  .attr("height", h)
+  .attr("height", h);
+
+d3.json("/myData.json", function (data) {
+  console.log(data)
+
+
+  d3.select("h4").text(data.name);
+})
